348c470ab92781e28bc5d184abeaa0aaf565134d
知念英弥 <s19013@std.it-college.ac.jp>
2023-06-05 17:30:33 +0900

    勝利判定

--- src/Component/Board.js
+++ src/Component/Board.js
@@ -13,12 +13,35 @@ export default class Board extends React.Component {
   handleClick(i){
     // 巻き戻しとかのため､古いのをコピーして保存する?
     const squares = this.state.squares.slice();
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ?'X':'O';
     // ステート書き換え
     this.setState({
       squares:squares,
       xisNext:!this.state.xIsNext,
     })
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 
     renderSquare(i) {
@@ -31,7 +54,13 @@ export default class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = this.calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner:' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
